4a3e19c63fa4432a137921be5b6ec927b907fe80
Nícolas Fernandes <167145187+niicfsz@users.noreply.github.com>
2024-08-27 19:48:27 -0300

    Código 2: Menu de agenda

--- Lista 2/incompleto.c
+++ Lista 2/incompleto.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct hora{
     int hr, min, seg;
@@ -9,49 +10,70 @@ struct data{
 };
 
 int main(){
-
-    struct hora a, data b;
-    char compromisso[100];
+    int op;
+    struct hora a;
+    struct data b;
+    char compromisso[100] = "";
+    int dataDefinida = 0, horarioDefinido = 0, compromissoDefinido = 0;
 
     do{
-            
-    printf("\n\n==========Menu==========\n");
-    printf("Definir data;");
-    printf("Definir horario;");
-    printf("Definir compromisso")
-    
-    switch(op){
-    case 1:
-    printf("Ano: ");
-    scanf("%d", &b.ano);
-    printf("Mes: ");
-    scanf("%d", &b.mes);
-    printf("Dia: ");
-    scanf("%d", &b.dia);
-    break;
-    
-    case 2:
-    printf("Hora: ");
-    scanf("%d", &a.hr);
-    printf("Minutos: ");
-    scanf("%d", &a.min);
-    printf("Segundos: ");
-    scanf("%d", &a.seg);
-    break;
-    
-    case 3:
-    printf("Digite o compromisso do dia: ");
-    
-    
-    default:
-    if(op == 0){
-    printf("\nSaindo...\n");
-            break;
-    } else{
-        printf("Opçao inexistente, por favor digite novamente.");
-        }
-            }
-    }while(op != 0);
+        printf("\n\n==========Menu==========\n");
+        printf("1. Definir data\n");
+        printf("2. Definir horário\n");
+        printf("3. Definir compromisso\n");
+        printf("4. Exibir informações definidas\n");
+        printf("0. Sair\n\n");
+        printf("Escolha uma das opções a seguir e digite seu respectivo número: ");
+        scanf("%d", &op);
+        getchar();
 
-    
+        switch(op){
+            case 1:
+                printf("\n\nAno: ");
+                scanf("%d", &b.ano);
+                printf("\nMês: ");
+                scanf("%d", &b.mes);
+                printf("\nDia: ");
+                scanf("%d", &b.dia);
+                dataDefinida = 1;
+                break;
+
+            case 2:
+                printf("\n\nHora: ");
+                scanf("%d", &a.hr);
+                printf("\nMinutos: ");
+                scanf("%d", &a.min);
+                printf("\nSegundos: ");
+                scanf("%d", &a.seg);
+                horarioDefinido = 1;
+                break;
+
+            case 3:
+                printf("\n\nDigite o compromisso do dia: ");
+                fgets(compromisso, sizeof(compromisso), stdin);
+                compromisso[strcspn(compromisso, "\n")] = '\0';
+                compromissoDefinido = 1;
+                break;
+
+            case 4:
+                if (dataDefinida && horarioDefinido && compromissoDefinido) {
+                    printf("\nInformações Definidas:\n");
+                    printf("\n\nData: %02d/%02d/%d\n", b.dia, b.mes, b.ano);
+                    printf("\nHorário: %02d:%02d:%02d\n", a.hr, a.min, a.seg);
+                    printf("\nCompromisso: %s\n", compromisso);
+                } else {
+                    printf("\nVocê precisa definir a data, o horário e o compromisso antes de exibir as informações.\n");
+                }
+                break;
+
+            default:
+                if(op == 0){
+                    printf("\nSaindo...\n");
+                } else{
+                    printf("Opção inexistente, por favor digite novamente.\n");
+                }
+        }
+    } while(op != 0);
+
+    return 0;
 }
