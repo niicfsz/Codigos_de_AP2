1b22b43196ee903da6c6958b7b1ddd405ec0f2b4
Nícolas Fernandes <167145187+niicfsz@users.noreply.github.com>
2024-08-31 21:51:14 -0300

    Create 6° Código: Struct na struct.c

---
+++ Lista 2/6° Código: Struct na struct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+struct endereco{
+    char rua[100], cep[20];
+    int n;
+};
+
+struct cliente{
+    struct endereco dados;
+    char nome[100];
+    float saldo;
+};
+
+int main(){
+
+    struct cliente a;
+
+    strcpy(a.nome, "Nicolas Fernandes");
+    strcpy(a.dados.rua, "Rua 006");
+    a.dados.n = 1234;
+    strcpy(a.dados.cep, "758000-0");
+    a.saldo = 1250.00;
+
+    strcpy(a.nome, "Nilma Fernandes");
+    strcpy(a.dados.rua, "Rua BH 2");
+    a.dados.n = 567;
+    strcpy(a.dados.cep, "321654-32");
+    a.saldo = 1500.00;
+
+    printf("Nome: %s\n", a.nome);
+    printf("Rua: %s\n", a.dados.rua);
+    printf("Numero: %d\n", a.dados.n);
+    printf("CEP: %s\n", a.dados.cep);
+    printf("Saldo: %.2f\n", a.saldo);
+
+    return 0;
+}
